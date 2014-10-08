268393f8fe76cfe5df37e0a32864a4c7629b43e7
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-10-08 11:08:01 +0900

    apply formatter

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -12,25 +12,22 @@ stderr_path File.join(RAILS_ROOT, "log/unicorn.stderr.log")
 stdout_path File.join(RAILS_ROOT, "log/unicorn.stdout.log")
 
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and
+GC.respond_to?(:copy_on_write_friendly=) &&
   GC.copy_on_write_friendly = true
 check_client_connection false
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
+  defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
 
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
+    sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+    Process.kill(sig, File.read(old_pid).to_i)
   end
 end
 
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.establish_connection
 end
