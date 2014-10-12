10b6aee4bbce563f6b6b6d5291aa6296243b6f1c
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-10-12 12:25:30 +0900

    fix

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,5 +1,6 @@
 if Rails.env == 'development'
   require 'rack-mini-profiler'
+  p "#######################################################"
 
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
